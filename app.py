652e691ecab98fdaa59e1f37ef5a6a59cd9ac449
Ken Takagi <tkg214@gmail.com>
2017-07-07 13:16:20 -0700

    fix postback

--- app.py
+++ app.py
@@ -66,11 +66,13 @@ def send_postback(recipient_id, button_text):
         "recipient": {
             "id": recipient_id
         },
-        "buttons": [{
+        "message": {
+            "buttons": [{
             "type":"postback",
             "title":"Bookmark Item",
             "payload":"DEVELOPER_DEFINED_PAYLOAD"
-        }]
+            }]
+        }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
